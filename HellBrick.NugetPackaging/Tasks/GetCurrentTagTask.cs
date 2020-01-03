6bcb04f59012992bf5bf09c9c23c30fe48ffbdf4
HellBrick <hellbrick@outlook.com>
2020-01-03 22:01:03 +0300

    Added GetCurrentTagTask

---
+++ HellBrick.NugetPackaging/Tasks/GetCurrentTagTask.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using LibGit2Sharp;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace HellBrick.NugetPackaging.Tasks
+{
+	public class GetCurrentTagTask : Task
+	{
+		public string? RepositoryFolderPath { get; set; }
+
+		[Output]
+		public string? CurrentTag { get; private set; }
+
+		public override bool Execute()
+		{
+			if ( RepositoryFolderPath is null )
+			{
+				Log.LogError( $"{nameof( RepositoryFolderPath )} must be specified." );
+				return false;
+			}
+
+			using Repository repo = new Repository( RepositoryFolderPath );
+			CurrentTag = repo.Tags.FirstOrDefault( t => t.Target.Sha == repo.Head.Tip.Sha )?.FriendlyName;
+			return true;
+		}
+	}
+}
