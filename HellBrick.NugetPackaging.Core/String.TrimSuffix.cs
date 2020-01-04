6c8e2427bae318c690e8b2c6ef92f9bf910d2191
HellBrick <hellbrick@outlook.com>
2020-01-04 16:43:47 +0300

    Added String.TrimSuffix()

---
+++ HellBrick.NugetPackaging.Core/String.TrimSuffix.cs
@@ -0,0 +1,10 @@
+﻿namespace HellBrick.NugetPackaging
+{
+	internal static partial class StringExtensions
+	{
+		public static string TrimSuffix( this string text, string suffix )
+			=> text.EndsWith( suffix )
+				? text.Substring( 0, text.Length - suffix.Length )
+				: text;
+	}
+}
