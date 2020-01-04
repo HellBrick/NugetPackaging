57f9aad84e9baf869950fc61202450ba5435ae1b
HellBrick <hellbrick@outlook.com>
2020-01-04 16:41:39 +0300

    Added IEnumerable<T>.ToHashSet()

---
+++ HellBrick.NugetPackaging.Core/Enumerable.ToHashSet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HellBrick.NugetPackaging
+{
+	internal static partial class EnumerableExtensions
+	{
+		public static HashSet<T> ToHashSet<T>( this IEnumerable<T> sequence ) => sequence.ToHashSet( EqualityComparer<T>.Default );
+		public static HashSet<T> ToHashSet<T>( this IEnumerable<T> sequence, IEqualityComparer<T> comparer ) => new HashSet<T>( sequence, comparer );
+	}
+}
